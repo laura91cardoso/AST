a6d7747a5c1f7daf3de9404715ba75050f6c8568
laura91cardoso <a19239@alunos.ipca.pt>
2021-11-25 15:39:50 +0000

    Update TP_Andre_Helder_Laura.R
    Co-Authored-By: Andre-Nogueira <94172688+Andre-Nogueira@users.noreply.github.com>
    Co-Authored-By: Hélder Barbosa <36603124+MalhaVacas@users.noreply.github.com>

--- TP_Andre_Helder_Laura.R
+++ TP_Andre_Helder_Laura.R
@@ -8,9 +8,40 @@ class(UKgas)
 #Representação gráfica da série temporal
 autoplot(UKgas)
 
-UKga
-autoplot(UKgas, xlab = "Anos", ylab = "Milhões", main = "Consumo de gás no Reino Unido") 
+UKgas
+autoplot(UKgas, xlab = "Anos", ylab = "Milhões", main = "Consumo de gás no Reino Unido entre 1960 e 1986") 
 
 ggAcf(UKgas, main="Correlograma", lag=12)
 
-mk.test(UKgas)
+######################################################################
+###############PROCESSOS ESTOCÁSTICOS#################################
+
+#função para aplicar no teste Mann-Kendall com sazonalidade
+
+smk.test(UKgas)
+plot(nottem) #1º testar a sazonalidade
+autoplot(nottem) #tem sazonalidade
+class(nottem) #para sabermos se é ts
+
+
+
+
+#2º- definir as hipóteses:
+# H0: Não há tendeência
+# H1: Há tendência
+
+#3º Estatística de teste : z= 2.0919
+#consideramos alfa= 0.05
+# valor p= .03654 < 0.05 , rejeita-se H0, logo há tendência
+# Como s= 224 >0 , a tendência é crescente.
+
+
+#TESTE DE MANN-WHITNEY-PETTITT - MUDANÇA NA VARIÂNCIA
+
+pettitt.test() #função
+
+autoplot(ts(PagesData))
+pettitt.test(PagesData)
+s.res<-pettitt.test(PagesData) # variável criada s. res para guardar os valores do teste
+s.res 
+
